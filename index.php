8e46db96508ceff298a6bda0a40c2ac3d5a56811
Raj Patel <rajspatel1297@gmail.com>
2017-01-26 10:59:15 +0530

    improved design

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 <meta content="width=device-width, initial-scale=1, user-scalable=no" name="viewport">
 <title>FAPP</title>
 
-
 <!-- Google icon -->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
