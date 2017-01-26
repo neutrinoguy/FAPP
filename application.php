a389b25b5ec7b5a1bff8d47b7cfccae0ac1071a7
Raj Patel <rajspatel1297@gmail.com>
2017-01-26 10:42:22 +0530

    improvement in design

--- application.php
+++ application.php
@@ -5,18 +5,18 @@
 <body>
 <div dir="ltr" style="text-align: left;" trbidi="on">
 To,<br />
-<?php echo $_POST["to"]; ?><br />
-<?php echo $_POST["department"]; ?> Department,<br />
+<?php echo $_POST["text-to"]; ?><br />
+<?php echo $_POST["text-department"]; ?> Department,<br />
 SVBIT, Vasan.<br />
 <br />
-Date :<?php echo date("d/m/Y"); ?><br />
+Date :<?php echo date("m/d/Y"); ?><br />
 <div style="text-align: center;">
 <b>Subject: Application for Leave</b></div>
 <div style="text-align: center;">
 <br /></div>
 Respected Sir/Madam,<br />
 <br />
-Myself,<?php echo $_POST["name"]; ?> from <?php echo $_POST["department"]; ?> class En-no <?php echo $_POST["en-no"]; ?>.I have to attend a <?php echo $_POST["reason"]; ?> from <?php echo $_POST["from_date"]; ?> to <?php echo $_POST["to_date"]; ?>.So, I request you to grant my leave for given dates.
+Myself,<?php echo $_POST["text-name"]; ?> from <?php echo $_POST["text-department"]; ?> class En-no <?php echo $_POST["text-e-no"]; ?>.I have to attend a <?php echo $_POST["text-reason"]; ?> from <?php echo $_POST["datepicker-start"]; ?> to <?php echo $_POST["datepicker-end"]; ?>.So, I request you to grant my leave for given dates.
 <br />
 <br />
 Awaiting your favourable reply,Thank you.<br />
@@ -26,7 +26,7 @@ Awaiting your favourable reply,Thank you.<br />
 </div>
 Yours Faithfully,<br />
 <?php echo $_POST["name"]; ?><br />
-<?php echo date("d/m/Y"); ?></div>
+<?php echo date("m/d/Y"); ?></div>
 </body>
 </html>
 
--- form.php
+++ form.php
@@ -1,137 +1,155 @@
-<!DOCTYPE html>
-<html lang="en"><head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="img/favicon.ico">
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>FAPP</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    
-    <!-- Custom styles for this template -->
-    <link href="css/main.css" rel="stylesheet">
+<meta content="width=device-width, initial-scale=1, user-scalable=no" name="viewport">
+<title>FAPP</title>
 
 
-  </head>
+<!-- Google icon -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <body>
+<!-- Bootstrap css -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- Propeller css -->
+<link rel="stylesheet" type="text/css" href="css/propeller.min.css">
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        
-          <a class="navbar-brand" href="#" >Fast Application-FAPP</a>
-        
+<!-- Propeller theme css-->
+<link rel="stylesheet" type="text/css" href="css/propeller-theme.css" />
+
+<!-- Propeller admin theme css-->
+<link rel="stylesheet" type="text/css" href="css/propeller-admin.css">
+
+<!-- favicon --> 
+	<link rel="icon" href="http://propeller.in/assets/images/favicon.ico" type="image/x-icon">
+	
+	<!-- Bootstrap --> 
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css" rel="stylesheet" /> 
+	
+	<!-- Example docs (CSS for helping component example file)-->
+	<link href="http://propeller.in/docs/css/example-docs.css" type="text/css" rel="stylesheet" />
+	
+	<!-- Propeller card (CSS for helping component example file)-->
+	<link href="http://propeller.in/components/card/css/card.css" type="text/css" rel="stylesheet"/>
+
+	<!-- Propeller typography -->
+	<link href="http://propeller.in/components/typography/css/typography.css" type="text/css" rel="stylesheet" />
+	
+	<!-- Propeller textbox -->
+	<link href="http://propeller.in/components/textfield/css/textfield.css" type="text/css" rel="stylesheet"/>
+
+	<!-- Select2 css -->
+	<link href="css/select2.min.css" type="text/css" rel="stylesheet"/>
+	<link href="css/select2-bootstrap.css" type="text/css" rel="stylesheet"/>
+	
+	<!-- Propeller Select2 css -->
+	<link href="css/pmd-select2.css" type="text/css" rel="stylesheet"/>
+
+	<link href="css/button.css" type="text/css" rel="stylesheet"/>
+
+  <!-- Google Icon Font -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="http://propeller.in/components/icons/css/google-icons.css" type="text/css" rel="stylesheet" />
+
+	<!-- Propeller textbox -->
+	<link href="http://propeller.in/components/textfield/css/textfield.css" type="text/css" rel="stylesheet"/>
+
+	<!-- Propeller bootstrap datetimepicker -->
+	<link href="http://propeller.in/components/datetimepicker/css/bootstrap-datetimepicker.css" type="text/css" rel="stylesheet" />
+	
+	<!-- ProDatatable -->
+	<link href="css/pmd-datetimepicker.css" type="text/css" rel="stylesheet"/>
+
+<!-- Styles Ends -->
+</head>
+
+<body>
+<!-- Header Starts -->
+<!--Start Nav bar -->
+<nav class="navbar navbar-inverse navbar-fixed-top pmd-navbar pmd-z-depth">
+
+	<div class="container-fluid">
+		<div class="pmd-navbar-right-icon pull-right navigation">
+
+			
+	
+		</div>
+	</div>
+
+</nav><!--End Nav bar -->
+<!-- Header Ends -->
+<!--Form-->
+
+<!--<body background="../img/2.jpg">-->
+
+<form action="application.php" method="post">
+<div class="pmd-content pmd-content-custom" id="content"> 
+    <div class="container">
+		
+		<!-- To Input -->
+        <div class="form-group pmd-textfield">
+            <label for="text-to" class="control-label">Name</label>
+            <input type="text" id="text-to" class="form-control">
+        </div>
+    <!-- Department Input -->
+        <div class="form-group pmd-textfield">
+            <label for="text-department" class="control-label">Department Name</label>
+            <input id="text-department" class="form-control" type="text">
+        </div>
+<!--Name Text Input -->
+        <div class="form-group pmd-textfield">
+            <label for="text-name" class="control-label">Name</label>
+            <input id="text-name" class="form-control" type="text">
+        </div>
+
+<!-- E -no -->
+        <div class="form-group pmd-textfield">
+            <label for="text-e-no" class="control-label">Enrollment No</label>
+            <input id="text-e-no" class="form-control" type="text">
+        </div>
+<!-- Reason Input -->
+        <div class="form-group pmd-textfield">
+            <label for="text-reason" class="control-label">Reason</label>
+            <input id="text-reason" class="form-control" type="text" value="<?php echo $_POST["reason"]; ?>">
+        </div>
+<!-- date Input -->
+        <div class="row">
+	          <div class="col-sm-6"> 
+		            <div class="form-group pmd-textfield pmd-textfield-floating-label">
+			              <label class="control-label" for="regular1">Start Date</label>
+			              <input type="text" class="form-control" id="datepicker-start">
+		            </div>
+	          </div>
+	          <div class="col-sm-6"> 
+		              <div class="form-group pmd-textfield pmd-textfield-floating-label">
+			                <label class="control-label" for="regular1">End Date</label>
+			                <input type="text" class="form-control" id="datepicker-end">
+		              </div>
+	          </div>
         </div>
       </div>
-    </nav>
-
-    <div class="container">
-
-      <div class="starter-template">
-         
-         <h1>Simple Leave application</h1>
-
-
-            <form class="form-horizontal" role="form" method="post" action="application.php">
-          <div class="form-group">
-            <label for="to" class="col-sm-2 control-label">To</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="to" name="to" placeholder="The Class Co-ordinator" value="">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="department" class="col-sm-2 control-label">Department</label>
-            <div class="col-sm-10">
-              <input type="department" class="form-control" id="department" name="department" placeholder="Computer/IT" value="">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="name" class="col-sm-2 control-label">Name</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="name" name="name" placeholder="Raj Patel" value="">
-            </div>
-          </div>
-
-          
-          <div class="form-group">
-            <label for="en-no" class="col-sm-2 control-label">Enrollment-No</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="en-no" name="en-no" placeholder="140750107037" value="">
-            </div>
-          </div>
-
-	
-          <div class="form-group">
-            <label for="reason" class="col-sm-2 control-label">Reason</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="reason" name="reason" placeholder="Tech event" value="<?php echo $_POST["reason"]; ?>" >
-            </div>
-          </div>
-
-
-
-
-          <div class="form-group">
-            <label for="from_date" class="col-sm-2 control-label">From</label>
-            <div class="col-sm-10">
-              <input type="date" class="form-control" id="from_date" name="from_date" placeholder="12/01/17" value="">
-            </div>
-          </div>
-
-
-          <div class="form-group">
-            <label for="to_date" class="col-sm-2 control-label">To</label>
-            <div class="col-sm-10">
-              <input type="date" class="form-control" id="to_date" name="to_date" placeholder="18/01/17" value="">
-            </div>
-          </div>
-
-          
-      
-
-
-          <div class="form-group">
-            <div class="col-sm-10 col-sm-offset-2">
-              <input id="submit" name="submit" type="submit" value="Generate" class="btn btn-primary">
-            </div>
-          </div>
-          
+   <button type="submit" class="btn pmd-ripple-effect btn-primary"> Next </button >
+                
+</div> <!--container end --> 
 </form>
 
 
-        
-      </div>
+<!-- Scripts Starts -->
+<script src="js/jquery-1.12.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/propeller.min.js"></script>
 
-    </div><!-- /.container -->
+<!-- Propeller textfield js -->
+<script type="text/javascript" src="http://propeller.in/components/textfield/js/textfield.js"></script>
 
+<!-- Datepicker moment with locales -->
+<script type="text/javascript" language="javascript" src="http://propeller.in/components/datetimepicker/js/moment-with-locales.js"></script>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.js"></script>
-    
+<!-- Propeller Bootstrap datetimepicker -->
+<script type="text/javascript" language="javascript" src="http://propeller.in/components/datetimepicker/js/bootstrap-datetimepicker.js"></script>
 
-  <iframe id="iframe-widget" allowfullscreen="" src="" width="300" height="250" frameborder="0"></iframe>
-
-</div>
 
 </body>
 </html>
